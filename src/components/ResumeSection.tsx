c3529261030455c81e597d4623d19641d74ca621
Mintly20 <mintly.2026@gmail.com>
2026-02-13 18:40:40 +0530

    Update ResumeSection.tsx

--- src/components/ResumeSection.tsx
+++ src/components/ResumeSection.tsx
@@ -42,7 +42,7 @@ const ResumeSection = () => {
                 className="pulse-glow"
                 asChild
               >
-                <a href="./Push Resume.pdf" download>
+                <a href="https://drive.google.com/file/d/1yVZ0fLkV9kuFV8yQ3h6KzWKL9j7EHNgN/view?usp=sharing" download>
                   <Download className="mr-2 h-5 w-5" />
                   Download Resume
                 </a>
